776e4284280a8d6574142acc9320bd33bb856a63
ljw2669 <dlwodnnjs@gmail.com>
2023-11-08 20:54:00 +0900

    OrderList 수정

--- src/component/OrderList.jsx
+++ src/component/OrderList.jsx
@@ -1,5 +1,6 @@
 import OrderListComp from "../style/OrderListStyle";
 import { Link } from "react-router-dom";
+import OrderImgSwiper from "../util/OrderImgSwiper";
 const Order = ({ e }) => {
   return (
     <>
@@ -8,7 +9,9 @@ const Order = ({ e }) => {
           <div className="ordersecBorder1">
             <h2>주문번호 : {e.orderNumber}</h2>
             <div className="orderImgBorder">
-              {<img src={e.imgFullUrl} alt="오더 이미지" />}
+              <div className="swiperWrapper">
+                <OrderImgSwiper imgListUrl={e.imgListUrl} />
+              </div>
             </div>
           </div>
           <div className="ordersecBorder2">
@@ -30,12 +33,11 @@ const Order = ({ e }) => {
             </div>
           </div>
           <Link to="/MyPage/OrderInfo">
-          <div className="orderBtn">
-            <button>자세히</button>
-          </div>        
+            <div className="orderBtn">
+              <button>자세히</button>
+            </div>
           </Link>
         </div>
-
       </div>
     </>
   );
@@ -49,8 +51,28 @@ const OrderList = () => {
       repairItem: "가방",
       repairDetail: "가죽 복원",
       requestDate: "2023.10.29",
-      imgFullUrl:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_full_bag.jpeg?alt=media&token=b0dcc4bd-64aa-40e1-b70d-d7e8b84e29e8",
+      imgListUrl: [
+        {
+          type: "imgFullUrl",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_full_bag.jpeg?alt=media&token=b0dcc4bd-64aa-40e1-b70d-d7e8b84e29e8",
+        },
+        {
+          type: "imgDetail1",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_3_detail_bag.jpeg?alt=media&token=68d7eea9-58c4-4172-87ea-416ce980f696",
+        },
+        {
+          type: "imgDetail2",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_1_detail_bag.jpeg?alt=media&token=60ea37e3-1669-422a-bb9d-c15f67c27d6b",
+        },
+        {
+          type: "imgDetail3",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_2_detail_bag.jpeg?alt=media&token=700a7d52-2056-41ef-a2fe-e3bfcba26f57",
+        },
+      ],
     },
     {
       orderNumber: "231030100024",
@@ -58,8 +80,28 @@ const OrderList = () => {
       repairItem: "가방",
       repairDetail: "가죽 복원",
       requestDate: "2023.10.30",
-      imgFullUrl:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/15_full_bag.jpeg?alt=media&token=6f786c62-44a1-42f0-b94a-c4e5dc301293",
+      imgListUrl: [
+        {
+          type: "imgFullUrl",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_full_bag.jpeg?alt=media&token=b0dcc4bd-64aa-40e1-b70d-d7e8b84e29e8",
+        },
+        {
+          type: "imgDetail1",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_3_detail_bag.jpeg?alt=media&token=68d7eea9-58c4-4172-87ea-416ce980f696",
+        },
+        {
+          type: "imgDetail2",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_1_detail_bag.jpeg?alt=media&token=60ea37e3-1669-422a-bb9d-c15f67c27d6b",
+        },
+        {
+          type: "imgDetail3",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_2_detail_bag.jpeg?alt=media&token=700a7d52-2056-41ef-a2fe-e3bfcba26f57",
+        },
+      ],
     },
     {
       orderNumber: "231016100010",
@@ -67,8 +109,28 @@ const OrderList = () => {
       repairItem: "지갑",
       repairDetail: "가죽 교체",
       requestDate: "2023.10.16",
-      imgFullUrl:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_full_wallet.jpg?alt=media&token=0bbf843a-06a9-4f85-9f19-ba0aa05b9ed6",
+      imgListUrl: [
+        {
+          type: "imgFullUrl",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_full_bag.jpeg?alt=media&token=b0dcc4bd-64aa-40e1-b70d-d7e8b84e29e8",
+        },
+        {
+          type: "imgDetail1",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_3_detail_bag.jpeg?alt=media&token=68d7eea9-58c4-4172-87ea-416ce980f696",
+        },
+        {
+          type: "imgDetail2",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_1_detail_bag.jpeg?alt=media&token=60ea37e3-1669-422a-bb9d-c15f67c27d6b",
+        },
+        {
+          type: "imgDetail3",
+          imgUrl:
+            "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/14_2_detail_bag.jpeg?alt=media&token=700a7d52-2056-41ef-a2fe-e3bfcba26f57",
+        },
+      ],
     },
   ];
   return (
@@ -77,10 +139,6 @@ const OrderList = () => {
         <div className="container">
           <div className="orderHeader">
             <h2>주문현황</h2>
-            {/* {orderData.map((e) => (
-              <Order key={e.orderNumber} e={e} />
-            ))} */}
-
             <div className="orderBox">
               <div className="couponBox">
                 {orderData.map((e) => (
